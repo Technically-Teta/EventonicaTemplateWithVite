64c61b0dfa2f1d2f9b1d0e186a80a61189f0fcee
tushort <samquioh@gmail.com>
2023-09-11 23:44:58 -0700

    add tutorial info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css';
 import Events from './components/events';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [events, setEvents] = useState(false);
   return (
     <div className="App">
     <h1>Techtonica 2023 H2 events</h1>
