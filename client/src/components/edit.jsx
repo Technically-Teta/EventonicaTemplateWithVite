d1c0afbb4e0cba8b3524e48a587085407d04ed4b
tushort <samquioh@gmail.com>
2023-09-14 11:52:46 -0700

    create edit component and add code to include an edit button.

---
+++ client/src/components/edit.jsx
@@ -0,0 +1,42 @@
+
+
+function EditUserForm(props){
+//initalizes state to store user data
+const [user, setUser] = useState(props.initialData);
+
+//updates state with user data
+const handleInputChange = (event) => {   
+//destrutures user data extracts  properties the event object has
+    const {name, value} = event.target;
+// this will update the state field with the new value
+setUser({...user, [name]: value});
+
+}
+
+const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // this is a callback function that will hand the updated data   
+    props.onSubmit(user);
+};
+
+return (
+
+<form onSubmit={handleFormSubmit}>
+    <div>
+    <label htmlFor="title">title: </label>
+    <input type="text" id="name" name="title" value={user.name} onChange={handleInputChange}/>
+    </div>
+    <div>
+    <label htmlFor="location">location: </label>
+    <input type="text" id="name" name="title" value={user.name} onChange={handleInputChange}/>
+    </div>
+    <button type="submit">Submit</button>
+</form>    
+
+)
+
+
+
+}
+
+
