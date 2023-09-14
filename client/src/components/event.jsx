c92a2a17f2a6245c5230e7ea5c2314434e5ef135
tushort <samquioh@gmail.com>
2023-09-13 23:18:10 -0700

    copypaste event page to file. includes delete function

--- client/src/components/event.jsx
+++ client/src/components/event.jsx
@@ -1,22 +1,45 @@
 import Card from 'react-bootstrap/Card';
+import { confirmAlert } from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; 
+import moment from 'moment';
 
 const EventCard = (props) => {
-    //HOW DO I ADD A TIME TEMPLATE?
+
+    const handleDelete = () => {
+        //localhost:8080/api/events/5
+        const idToDelete = props.event.id;
+        confirmAlert({
+            title: 'Confirm to Delete Event',
+            message: `Are you sure to delete this event: ${props.event.title}?`,
+            buttons: [
+              {
+                label: 'Yes',
+                onClick: () => props.onDelete(idToDelete)
+              },
+              {
+                label: 'No',
+                onClick: () => console.log('Click No')
+              },
+              {
+                label: 'Maybe?',
+                onClick: () => console.log('Click Maybe')
+              }
+            ]
+          });
+        }
+        //props.onDelete(idToDelete)
+
 
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Date: {!props.time ? "TBD" : null}</Card.Subtitle>
+                <Card.Title>{props.event.title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Date: {!props.event.eventtime ? "TBD" :moment(props.event.eventtime).format('MMMM Do, YYYY')}</Card.Subtitle>
                 <Card.Text>
-                    Location: {props.location}
-                    <br />
-                    Description : {props.eventdescription}
-                    <br />
-                    Category: {props.category}
-                    <br />
-
+                    Location: {props.event.location}
                 </Card.Text>
+                <button onClick={handleDelete}>Delete</button>
+                <button>Edit</button>
             </Card.Body>
         </Card>
     )
